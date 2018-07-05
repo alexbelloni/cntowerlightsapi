077a96873e8ef085c060d239305082ca8c6b64ea
Alexandre Belloni Aves <alexbelloni@outlook.com>
2018-07-05 15:23:26 -0400

    Access-Control-Allow-Origin

--- app.js
+++ app.js
@@ -6,14 +6,14 @@ app.use(express.static(__dirname + '/public'))
 const routes = require('./routes/api');
 
 app.use(routes);
-/*
+
 app.use(function (req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.setHeader('Access-Control-Allow-Methods', 'POST, GET, PATCH, DELETE, OPTIONS');
   next();
 });
-*/
+
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
 });
