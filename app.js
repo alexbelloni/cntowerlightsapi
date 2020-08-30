d1b42433113f2d81f7ae1e375630aec85a243e92
Alexandre Belloni Alves <alexbelloni@outlook.com>
2020-08-30 17:56:35 -0400

    added Access-Control-Allow-Origin

--- app.js
+++ app.js
@@ -9,14 +9,14 @@ app.use(cors());
 const routes = require('./routes/api');
 
 app.use(routes);
-/*
+
 app.use(function (req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.setHeader('Access-Control-Allow-Methods', 'POST, GET, PATCH, DELETE, OPTIONS');
   next();
 });
-*/
+
 app.listen(app.get('port'), function() {
   console.log("Node app is running at http://localhost:" + app.get('port'))
 });
